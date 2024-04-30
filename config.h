445cf387d9111eb95f5f56d0133b3b54702bd935
ProMaster_4 <codeninja47@gmail.com>
2024-04-30 16:42:06 +0100

    bottom bar

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int refresh_rate = 240;
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "BlexMono Nerd Font:size=14" };
 static const char dmenufont[]       = "BlexMono Nerd Font:size=14";
 static const char col_gray1[]       = "#222222";
